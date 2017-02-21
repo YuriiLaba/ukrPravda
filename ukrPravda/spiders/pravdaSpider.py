2357b9a4e2430baf8a05fec82700ac936d6fb1a9
YuriyVilnitskiy <laba@ucu.edu.ua>
2017-02-21 15:14:14 +0200

    Make scraper which find main news on the site called "Ukrainian true"

--- ukrPravda/spiders/pravdaSpider.py
+++ ukrPravda/spiders/pravdaSpider.py
@@ -1,20 +1,26 @@
 import scrapy
+#for i in (response.css("div.article__title a::text").extract()): print(i.encode('utf-8'))
+#for i in response.css('title::text').extract(): print (i.encode('utf -8'))
 
-
-class QuotesSpider(scrapy.Spider):
+class PravdaSpider(scrapy.Spider):
     name = "pravdaSpider"
 
     def start_requests(self):
         urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
+            'http://www.pravda.com.ua/news/'
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        file_name = open('Main News.html','w')
+        main_news = response.css(".news.news_all .article.article_bold")
+
+        for i in (main_news.css(" .article__title a::text").extract()):
+            file_name.write(i.encode('utf-8'))
+            file_name.write('\n')
+
+        file_name.close()
+
+
+
